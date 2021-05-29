c079b48621894b566d7333c5c6955331643ebe83
Alex93pa <Ale.pug2@gmail.com>
2021-05-29 15:45:16 +0200

    fine

---
+++ script/script.js
@@ -0,0 +1,68 @@
+new Vue ({
+    el: "#app",
+    data: {
+        footerList1: [
+        {
+            text: "Feedback",
+            link: "#",
+        },
+        {
+            text: "Survey",
+            link: "#",
+        },
+        {
+            text: "Reviews",
+            link: "#",
+        },
+        {
+            text: "Praise",
+            link: "#",
+        },
+        {
+            text: "Slack integration",
+            link: "#",
+        },
+        {
+            text: "Schedule a demo",
+            link: "#",
+        },
+        {
+            text: "Pricing",
+            link: "#",
+        }
+        ],
+        footerList2: [
+            {
+                text: "Feedback",
+                link: "#",
+            },
+            {
+                text: "Survey",
+                link: "#",
+            },
+            {
+                text: "Reviews",
+                link: "#",
+            },
+            {
+                text: "Praise",
+                link: "#",
+            },
+            ],
+            footerList3: [
+                {
+                    text: "Feedback",
+                    link: "#",
+                },
+                {
+                    text: "Survey",
+                    link: "#",
+                },
+                {
+                    text: "Reviews",
+                    link: "#",
+                },
+                
+                ],
+    }
+})
